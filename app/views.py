e99990faa3ce8954e79ee1f141bb10ddec866ba0
vivshaw <hvivianshaw@gmail.com>
2017-02-05 02:18:04 -0500

    Generate viewable seed phrase for random seeds

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import re
 from app import app
 from flask import render_template, request
+from random import randint
 from generative_net import GenerativeNetwork
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,9 +12,17 @@ def index():
                                title="0x53 48 41 4b 45")
 
     if request.method == 'POST':
-        seed_pattern = net.make_seed()
-        message = net.generate(seed_pattern)
+        seed_phrase = request.form['seed']
+        seed = net.make_seed(seed_phrase)
+        message = net.generate(seed)
+
+        if not seed_phrase:
+            word_list = seed.split(" ")
+            i = randint(1, len(word_list) - 3)
+            words = word_list[i:i + 3]
+            seed_phrase = " ".join(words)
 
         return render_template('index.html',
                                title="0x53 48 41 4b 45",
-                               message=message)
+                               message=message,
+                               seed=seed_phrase)
