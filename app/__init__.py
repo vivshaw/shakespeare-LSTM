7483ef5633230febc98b8308c31068438cb04d09
vivshaw <hvivianshaw@gmail.com>
2017-02-06 01:33:20 -0500

    Tweak config so Heroku will load from env

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config.from_object('config')
+if os.environ.get('HEROKU') is None:
+    app.config.from_object('config')
+else:
+    app.secret_key = os.environ.get('SECRET_KEY')
 
 from app import views
 
